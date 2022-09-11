aff4fc395055d033f0c80f2d2ab0ab7da7f6a2a8
Michael <mickbuck@gmail.com>
2022-09-11 17:38:43 +1200

    Moved icons, changed main name

--- byartist.php
+++ byartist.php
@@ -1,22 +1,12 @@
 <?php
     include("include/config.php");
-    if (isset($_POST['btn']))
-    {
-      $date=$_POST['idate'];
-      $q="SELECT * FROM album order by UPPER(LTRIM(Replace(name, 'The', '')));";
-      $query=mysqli_query($sql,$q);
-    } 
-	else 
 	{
-        //$id = (int)$_GET['id'];$path = parse_url($url, PHP_URL_PATH);
-        //$pathFragments = explode('/', $path);
-        //$end = end($pathFragments);
 		$id = ($_GET["id"]); ?>
 		<?php
         $q= "SELECT * FROM album Where artist_id = $id order by UPPER(LTRIM(Replace(name, 'The', '')));";
-      
+        
       // $q= "SELECT * FROM album order by artist_id;";
-     $query=mysqli_query($sql,$q);
+     $query=mysqli_query($sql,$q); 
     }
 ?>
 <html>
@@ -28,10 +18,10 @@
     </head>
     <body>
         <h1>Albums</h1>
-        <a href="addalbum.php">Add New album</a>
+
         <div class="container mt-5"> 
             <div class="row mt-4">
-            <h5 class="card-title"><?php echo $qq['name']; ?></h5>     
+           
              <?php
                   while ($qq=mysqli_fetch_array($query)) 
                   {
@@ -41,21 +31,26 @@
                         <div class="card-body">
                             <?php  
                                 $qart = $qq['artist_id'];
+                                $lart = $qq['record_label_id'];
                             ?>
                             <?php $art = "SELECT name FROM artist WHERE id='$qart'"; 
                                   $artist=mysqli_query($sql,$art); ?>
                             <?php 
                                   while ($arts=mysqli_fetch_array($artist)) 
                                   {
-                                    ?>
-
-                                    <h5 class="card-title"><?php echo $qq['name']; ?></h5>      
-                                    <h6 class="card-title"><?php echo $arts['name']; ?></h6>
-                            <?php 
-                                  }  
                             ?>
-                            <h5 class="card-title"><img src="<?php echo $qq['image'];?>" alt="<?php echo $qq['id'];?>" style="width:200px;height:200px;"></h5>
-			                <br>
+                                    <h5 class="card-title"><?php echo $qq['name']; ?></h5>      
+                                    <h6 class="card-title"><?php echo $arts['name']; ?></h6>                             
+                                    <h5 class="card-title"><img src="<?php echo $qq['image'];?>" alt="<?php echo $qq['id'];?>" style="width:200px;height:200px;"></h5>
+                                    <br>
+                                    <h6>Catalogue Number:</h6> <h7 class="card-title"><?php echo $qq['cat_number']; ?></h7><br><br>
+                                    <?php $lab = "SELECT * FROM record_label WHERE id='$lart'"; 
+                                        $label=mysqli_query($sql,$lab); 
+                                        $labs=mysqli_fetch_array($label)?>
+                                    <h6>Record Label:</h6> <h7 class="card-title"><?php echo $labs['name'] ; ?></h7>   
+                                    <?php 
+                                  }  
+                            ?> 
                         </div>
                       </div><br>
                 </div>
@@ -64,5 +59,6 @@
                 ?>
             </div>
        </div>
+       <a href="addalbum.php">Add New album</a>
     </body>
 </html>
