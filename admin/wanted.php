a0db61be1f9d9d34d182bfa0a9f32f2dbaf5d94b
Michael <mickbuck@gmail.com>
2022-12-29 20:12:12 +1300

    Added edit button to wanted page.

--- admin/wanted.php
+++ admin/wanted.php
@@ -1,12 +1,14 @@
 <?php
-    include("../include/config.php");
+    include("include/config.php");
 	{
 		$id = ($_GET["id"]); ?>
 		<?php
-        $q= "SELECT  artist.id, album.name AS albumname, artist.name AS artistname, album.image from album, artist where album.artist_id = artist.id and album.onorder not like '1' and wanted like '1' order by UPPER(LTRIM(Replace(album.name, 'The ', '')))";
+        $q= "SELECT  artist.id, album.id AS albumid, album.name AS albumname, artist.name AS artistname, album.image from album, artist where album.artist_id = artist.id and album.onorder not like '1' and wanted like '1' order by UPPER(LTRIM(Replace(album.name, 'The ', '')))";
         $wanted=mysqli_query($sql,$q);
     }
 ?>
+
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -27,7 +29,8 @@
                         <div class="card-body1">
                                 <h5 class="card-title">Title: <?php echo $qq['albumname']; ?></h5>
                                 <h6 class="card-title">Artist: <?php echo $qq['artistname']; ?></h6>
-                                <h5 class="card-title"><img src="<?php echo $qq['image'];?>" alt="<?php echo $qq['id'];?>" style="width:200px;height:200px;"></h5>
+                                <h5 class="card-title"><img src="<?php echo $qq['image'];?>" alt="<?php echo $qq['albumid'];?>" style="width:200px;height:200px;"></h5>
+                                <br><input type="button" onclick="window.location='admin/editalbum.php?id=<?php echo $qq['albumid'];?>'" class="Redirect" value="Click Here To Edit"/>
                                 <?php
                             ?>
                         </div>
