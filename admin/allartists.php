519ebf5824780ba86eeef4dc7cfe1813f6b790b8
Michael <mickbuck@gmail.com>
2023-01-05 23:41:40 +1300

    Added Wanted and On Order numbers

--- admin/allartists.php
+++ admin/allartists.php
@@ -1,7 +1,7 @@
 <?php
     include("../include/config.php");
 	{
-    $q = "SELECT  DISTINCT (artist.name), artist.id, artist.Image from artist WHERE id != '1037' order by UPPER(LTRIM(Replace(artist.name, 'The ', '')))";
+    $q = "SELECT  DISTINCT (artist.name), artist.id, artist.Image from album, artist where album.artist_id = artist.id order by UPPER(LTRIM(Replace(artist.name, 'The ', '')))";
     $query=mysqli_query($sql,$q);
     }
 ?>
@@ -10,7 +10,6 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>My Music by Artists</title>
-        <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
         <link rel="stylesheet" href="../css/style.css">
     </head>
     <body>
@@ -26,24 +25,45 @@
                 <div class="col-lg-3">
                     <div class="card">
                         <div class="card-body1">
-                             <?php  
-                                $qid = $qq['id'] ?>
-                            <?php $t = "SELECT * from album WHERE artist_id=$qid";
-                                 $cd = "SELECT * from album WHERE artist_id=$qid AND format = '1'";
-                                 $vinyl = "SELECT * from album WHERE artist_id=$qid AND format = '2'";
-                            ?>
+                             <?php $qid = $qq['id'] ?>
+                                <?php $t = "SELECT * from album WHERE artist_id=$qid";
+                                $cd = "SELECT * from album WHERE artist_id=$qid AND (onorder IS NULL OR onorder != '1')  AND (wanted IS NULL OR wanted != '1') AND FORMAT = '1'";
+                                $vinyl = "SELECT * from album WHERE artist_id=$qid AND (onorder IS NULL OR onorder != '1')  AND (wanted IS NULL OR wanted != '1') AND FORMAT = '2'";
+                                $order = "SELECT * from album WHERE artist_id=$qid AND onorder = '1'";
+                                $wanted = "SELECT * from album WHERE artist_id=$qid AND wanted = '1'";
+                                ?>
                             <h5 class="card-title"><?php echo $qq['name']; ?></h5>
                             <h6 class="card-subtitle mb-2 text-muted">Total: 
                             <?php if ($result = mysqli_query($sql, $t)) {
                                 $rowcount = mysqli_num_rows( $result );
                                 printf("  %d\n", $rowcount);
-                             } ?>  CD: <?php if ($result = mysqli_query($sql, $cd)) {
+                             } ?>
+                             <br>  CD: <?php if ($result = mysqli_query($sql, $cd)) {
                                 $rowcount = mysqli_num_rows( $result );
                                 printf("  %d\n", $rowcount);
                              } ?>  Vinyl: <?php if ($result = mysqli_query($sql, $vinyl)) {
                                 $rowcount = mysqli_num_rows( $result );
                                 printf("  %d\n", $rowcount);
-                             } ?></h6>
+                             } ?>  
+                             <?php if ($result = mysqli_query($sql, $order)) {
+                                $rowcount = mysqli_num_rows( $result );
+                                
+                             
+                             If ($rowcount > '0') { ?>
+                             On Order: <?php printf("  %d\n", $rowcount);
+                             } 
+                            }?>  
+
+                            <?php if ($result = mysqli_query($sql, $wanted)) {
+                                $rowcount = mysqli_num_rows( $result );
+                                
+                             
+                             If ($rowcount > '0') { ?>
+                             Wanted: <?php printf("  %d\n", $rowcount);
+                             } 
+                            }
+
+                            ?></h6>
     	        	        <h5 class="card-title">
                                 <a href="../byartist.php?id=<?php echo $qq['id'];?>"> <img src="<?php echo $qq['Image'];?>" alt="<?php echo $qq['name'];?>" style="width:200px;height:200px;"></a>
                             </h5>
