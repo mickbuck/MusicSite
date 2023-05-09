209ce1b9522be6d3d50a61f9aa1c87cd6d78f3d8
Michael <mickbuck@gmail.com>
2023-05-09 18:22:18 +1200

    order on wanted

--- admin/wanted.php
+++ admin/wanted.php
@@ -2,7 +2,7 @@
 include("../include/config.php"); {
     $id = ($_GET["id"]); ?>
 <?php
-    $q = "SELECT  artist.id, album.id AS albumid, album.name AS albumname, artist.name AS artistname, album.image from album, artist where album.artist_id = artist.id and album.onorder not like '1' and wanted like '1' order by UPPER(LTRIM(Replace(artistname, 'The ', '')))";
+    $q = "SELECT artist.id, album.id AS albumid, album.name AS albumname, artist.name AS artistname, album.image from album, artist where album.artist_id = artist.id and album.onorder not like '1' and wanted like '1' order by UPPER(LTRIM(Replace(artistname, 'The ', ''))), UPPER(LTRIM(Replace(album.name, 'The ', '')))";
     $wanted = mysqli_query($sql, $q);
 }
 ?>
