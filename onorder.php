ddb9058931c0689fe52440f43f3c1c29408c6e5c
Michael <mickbuck@gmail.com>
2023-07-09 13:26:16 +1200

    Fixed small bug

--- onorder.php
+++ onorder.php
@@ -1,31 +1,34 @@
 <?php
-    include("include/config.php");
-	{
-		$id = ($_GET["id"]); ?>
-		<?php
-        $q = "SELECT  DISTINCT (artist.name), album.name, artist.id, album.id AS albumid, album.image, album.onorder, album.dateordered, album.trackingnum, album.presale from album, artist where album.artist_id = artist.id And album.onorder LIKE '1' order by dateordered, artist.name, album.name";
-        $wanted = mysqli_query($sql, $q);
-    }
+include("include/config.php"); {
+    $id = ($_GET["id"]); ?>
+<?php
+    $q = "SELECT  DISTINCT (artist.name), album.name, artist.id, album.id AS albumid, album.image, album.onorder, album.dateordered, album.trackingnum, album.presale from album, artist where album.artist_id = artist.id And album.onorder LIKE '1' order by dateordered, artist.name, album.name";
+    $wanted = mysqli_query($sql, $q);
+}
 ?>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>On Order</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <br>
-        <h1>On Order</h1>
-        <div class="container mt-5">
-            <div class="row mt-4">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>On Order</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <br>
+    <h1>On Order</h1>
+    <div class="container mt-5">
+        <div class="row mt-4">
             <?php
-                  while ($qq=mysqli_fetch_array($wanted))
-                  {
-             ?>
+            while ($qq = mysqli_fetch_array($wanted)) {
+            ?>
+                <?php $ordered = $qq['dateordered'] ?>
+                <?php $tracking = $qq['trackingnum'] ?>
+                <?php $presale = $qq['presale'] ?>
                 <div class="col-lg-3">
                     <div class="card">
                         <div class="card-body1">
-                        <h5 class="card-title"><?php echo $qq['name']; ?></h5>
+                            <h5 class="card-title"><?php echo $qq['name']; ?></h5>
                             <h5 class="card-title"><img src="<?php echo $qq['image']; ?>" alt="<?php echo $qq['id']; ?>" style="width:200px;height:200px;"></h5>
                             <h6 class="card-title"><b>Date Ordered: </b><?php echo $ordered; ?></h6>
                             <?php if ($tracking > '0') { ?>
@@ -34,15 +37,14 @@
                                 <?php } ?>
                             <?php if ($presale > '0') {
                                 ?>
-                                <h6 class="card-title"><b>Release Date: </b><?php echo $presale; ?></h6>     
-                                <?php } ?>   
-
+                                <h6 class="card-title"><b>Release Date: </b><?php echo $presale; ?></h6>
+                                <?php } ?>                                
                         </div>
-                      </div><br>
+                    </div><br>
                 </div>
-                <?php } ?>
-                <br>
-            </div>
+            <?php } ?>
+            <br>
         </div>
-    </body>
+    </div>
+</body>
 </html>
