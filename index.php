825d1e600271402efa4dc623c08a14655a1b3f61
Michael <mickbuck@gmail.com>
2023-07-18 18:58:37 +1200

    Fixed formating

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
                     <div class="card">
                         <div class="card-body1">
                              <?php $qid = $qq['id'] ?>
-                                <?php $t = "SELECT * from album WHERE artist_id=$qid AND (wanted IS NULL OR wanted != '1') ";
+                                <?php $t = "SELECT * from album WHERE artist_id=$qid AND (wanted IS NULL OR wanted != '1') And album.format > '0'";
                                 $cd = "SELECT * from album WHERE artist_id=$qid AND (onorder IS NULL OR onorder != '1')  AND (wanted IS NULL OR wanted != '1') AND FORMAT = '1'";
                                 $vinyl = "SELECT * from album WHERE artist_id=$qid AND (onorder IS NULL OR onorder != '1')  AND (wanted IS NULL OR wanted != '1') AND FORMAT = '2'";
                                 $order = "SELECT * from album WHERE artist_id=$qid AND onorder = '1'";
