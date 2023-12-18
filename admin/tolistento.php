1398c06aaa3535e8f1015e55c3fbe8beb08d9519
Michael <mickbuck@gmail.com>
2023-12-18 18:59:26 +1300

    Fixed small error

--- admin/tolistento.php
+++ admin/tolistento.php
@@ -31,7 +31,7 @@ include("../include/config.php"); {
                         <div class="card-body1">
                             <?php $qid = $qq['id'] ?>
                             <h5 class="card-title"><?php echo $qq['name']; ?>
-                                <a href="byartist.php?id=<?php echo $qq['id']; ?>">
+                                <a href="../byartist.php?id=<?php echo $qq['id']; ?>">
                                 <img src="<?php echo $qq['Image']; ?>" alt="<?php echo $qq['name']; ?>" style="width:200px;height:200px;"> <br></a> -->
                                 <br><img src="emojis/<?php echo $rating ?>.png" style="width: 25px;height:25px;"><br>
                                 <br><input type="button" onclick="window.location='editartist.php?id=<?php echo $qq['id']; ?>'" class="Redirect" value="Click Here To Edit" />
