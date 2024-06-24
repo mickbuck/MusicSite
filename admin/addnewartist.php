89c84638b953aa72a720da8add1880e9103a1d31
Michael <mickbuck2015@gmail.com>
2024-06-25 00:50:25 +1200

    fixed typo

--- admin/addnewartist.php
+++ admin/addnewartist.php
@@ -16,7 +16,7 @@ if (isset($_POST['artist'])) {
 	if ($listenorder != '1') {
 		$listenorder = '0';
 	}
-	$sql_insert =  "INSERT IGNORE INTO artist (name,tolistento,listenorder,site) VALUES ('$artist','$tolisten','$site')";
+	$sql_insert =  "INSERT IGNORE INTO artist (name,tolistento,site) VALUES ('$artist','$tolisten','$site')";
 	#if (
         mysqli_query($sql, $sql_insert);#) {
         $sql_get = "Select id from artist where name like '$artist'";
