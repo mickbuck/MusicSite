978be6225e2b42351806144de4edb359518525f7
Michael <mickbuck2015@gmail.com>
2024-12-18 17:17:01 +1300

    added site links

--- admin/random.php
+++ admin/random.php
@@ -42,49 +42,57 @@ include("../include/config.php"); {
                                 <h6 class="card-title"><b>On Lidarr: </b>
                                     <?php echo $tracking; ?><img src="emojis/cross.png" style="height:25px;"><br>
                                 <?php } ?>
-                                <?php if ($officalsite > '0') {
-                                ?><a href="<?php echo $qq['officalsite']; ?>" target="_blank"><img src="../images/site.png" style="height:100px;"></a>
-                                <?php
-                                } ?>
-                                <?php if ($other > '0') {
-                                    if (str_contains($other, 'youtube')) {
-                                ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/youtube.png" style="height:100px;"></a>
+                                <?php 
+                                    if ($officalsite > '0') {
+                                        ?><a href="<?php echo $qq['officalsite']; ?>" target="_blank"><img src="../images/site.png" style="height:75px;"></a>
+                                        <?php
+                                    }
+                                    if (str_contains($qq['youtube'], 'youtube')) {
+                                        ?><a href="<?php echo $qq['youtube']; ?>" target="_blank"><img src="../images/youtube.png" style="height:75px;"></a>
+                                        <?php
+                                    }
+                                    if (str_contains($qq['facebook'], 'facebook')) {
+                                        ?><a href="<?php echo $qq['facebook']; ?>" target="_blank"><img src="../images/facebook.png" style="height:75px;"></a>
+                                        <?php
+                                    }
+                                    if (str_contains($qq['instagram'], 'instagram')) {
+                                        ?><a href="<?php echo $qq['instagram']; ?>" target="_blank"><img src="../images/instagram.png" style="height:75px;"></a>
                                     <?php
                                     }
-                                }
-                                if (str_contains($other, 'facebook')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/facebook.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'instagram')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/instagram.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'spotify')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/spotify.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'divas')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/dark-divas.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'wikipedia')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/wikipedia.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'linktr')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/linktr.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'archives')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/metal-archives.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                if (str_contains($other, 'bandcamp')) {
-                                    ?><a href="<?php echo $qq['site']; ?>" target="_blank"><img src="../images/bandcamp.png" style="height:100px;"></a>
-                                <?php
-                                }
-                                ?>
+                                    if (str_contains($qq ['spotify'], 'spotify')) {
+                                        ?><a href="<?php echo $qq ['spotify']; ?>" target="_blank"><img src="../images/spotify.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['divas'], 'divas')) {
+                                        ?><a href="<?php echo $qq ['divas']; ?>" target="_blank"><img src="../images/dark-divas.jpg" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['wikipedia'], 'wikipedia')) {
+                                        ?><a href="<?php echo $qq ['wikipedia']; ?>" target="_blank"><img src="../images/wikipedia.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['linktr'], 'linktr')) {
+                                        ?><a href="<?php echo $qq ['linktr']; ?>" target="_blank"><img src="../images/linktr.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['twitter'], 'twitter')) {
+                                        ?><a href="<?php echo $qq ['twiiter']; ?>" target="_blank"><img src="../images/twitter.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['twitter'], 'x.com')) {
+                                        ?><a href="<?php echo $qq ['twitter']; ?>" target="_blank"><img src="../images/twitter.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['musicarc'], 'metal-archives')) {
+                                        ?><a href="<?php echo $qq ['musicarc']; ?>" target="_blank"><img src="../images/metal-archives.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+                                    if (str_contains($qq ['bandcamp'], 'bandcamp')) {
+                                        ?><a href="<?php echo $qq ['bandcamp']; ?>" target="_blank"><img src="../images/bandcamp.png" style="height:75px;"></a>
+                                    <?php
+                                    }
+
+                                ?> 
                                 <br><input type="button" onclick="window.location='editartist.php?id=<?php echo $qq['id']; ?>'" class="Redirect" value="Click Here To Edit"  />
                             </h5>
                         </div>
