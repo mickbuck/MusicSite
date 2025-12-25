4ee6992736af400e9f339197cbe16d7af177020c
Michael <mickbuck@gmail.com>
2025-12-25 17:11:05 +1300

    Removed old links

--- admin/rated5.php
+++ admin/rated5.php
@@ -23,7 +23,6 @@ include("../include/config.php"); {
             <?php
             while ($qq = mysqli_fetch_array($query)) {
             ?>
-            <?php $lidarr = $qq['onladarr'] ?>
                 <div class="col-lg-3">
                     <div class="card">
                         <div class="card-body1">
@@ -31,14 +30,6 @@ include("../include/config.php"); {
                             <h5 class="card-title"><?php echo $qq['name']; ?>
                                 <a href="../byartist.php?id=<?php echo $qq['id']; ?>">
                                 <img src="<?php echo $qq['Image']; ?>" alt="<?php echo $qq['name']; ?>" style="width:98%;"> <br></a>
-                                <?php if ($lidarr > '0') { ?>
-                                <h6 class="card-title"><b>On Lidarr: </b>
-                                    <?php echo $tracking; ?><img src="emojis/tick.png" style="height:25px;"><br>
-                                <?php } ?>
-                                <?php if ($lidarr == '0') { ?>
-                                <h6 class="card-title"><b>On Lidarr: </b>
-                                    <?php echo $tracking; ?><img src="emojis/cross.png" style="height:25px;"><br>
-                                <?php } ?>
                                 <br><img src="emojis/<?php echo $qq['rating'] ?>.png" style="width: 25px;height:25px;"><br>
                                 <br><input type="button" onclick="window.location='editartist.php?id=<?php echo $qq['id']; ?>'" class="Redirect" value="Click Here To Edit" />
 
